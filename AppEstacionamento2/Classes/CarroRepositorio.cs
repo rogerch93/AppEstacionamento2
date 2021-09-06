9c6269b19e7ada49b39d13814b7124125bde7d14
rogerch93 <rogerinhochaves@hotmail.com>
2021-09-05 21:59:02 -0300

    Recriando App

---
+++ AppEstacionamento2/Classes/CarroRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using AppEstacionamento2.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEstacionamento2
+{
+    public class CarroRepositorio : ICarros<CarroPessoa>
+    {
+        private List<CarroPessoa> listaCarroPessoa = new List<CarroPessoa>();
+        public void Atualiza(int id, CarroPessoa objeto)
+        {
+            listaCarroPessoa[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaCarroPessoa[id].Exclui();
+        }
+
+        public void Insere(CarroPessoa objeto)
+        {
+            listaCarroPessoa.Add(objeto);
+        }
+
+        public List<CarroPessoa> Lista()
+        {
+            return listaCarroPessoa;
+        }
+
+        public int ProximoId()
+        {
+            return listaCarroPessoa.Count;
+        }
+
+        public CarroPessoa RetornaPorId(int id)
+        {
+            return listaCarroPessoa[id];
+        }
+    }
+}
---
+++ AppEstacionamento2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AppEstacionamento2
+{
+    class Program
+    {
+        static CarroRepositorio repositorio = new CarroRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = obterLista();
+
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        InserirCarros();
+                        break;
+                    case "2":
+                        ListarCarros();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+            }
+        }
+        
+        //Interface Visual do Sistema.
+        private static string obterLista()
+        {
+            Console.WriteLine();
+            Console.WriteLine("     " + "***SITEMA PARA ESTACIONAMENTO***");
+            Console.WriteLine("     " + "----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("     " + " Informe qual opção desejada");
+            Console.WriteLine("     " + "==================================");
+            Console.WriteLine("     " + " 1 - Inserir novo veiculo:");
+            Console.WriteLine("     " + "==================================");
+            Console.WriteLine("     " + " 2 - Listar veiculos:");
+            Console.WriteLine("     " + "==================================");
+            Console.WriteLine("     " + " X - Sair");
+            Console.WriteLine();
+
+            string opcaoCarros = Console.ReadLine();
+            return opcaoCarros;
+        }
+    }
+}
